c989a4c36363f5c3fea27bfe26ceb0feaeed283d
Norberto Hideaki Enomoto <norberto.enomoto@gmail.com>
2020-02-28 17:12:09 -0300

    Added comments

--- src/main/java/com/example/springdatajpa/repository/UserRepository.java
+++ src/main/java/com/example/springdatajpa/repository/UserRepository.java
@@ -6,7 +6,7 @@ import com.example.springdatajpa.model.User;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
-
+// Norberto Enomoto
 public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
